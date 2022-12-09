6a6904cf90e004b586a5aa28bfe30d246edc2196
amirrezaask <raskarpour@gmail.com>
2022-12-09 17:47:04 +0330

    day 8 part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,5 +1,44 @@
+fn scenic_score(grid: &Vec<Vec<u32>>, pos: (usize, usize)) -> u32 {
+    let height = grid[pos.0][pos.1];
+    let (left, right) = grid[pos.0].split_at(pos.1);
+    let mut left = left.to_vec();
+    let mut right = right[1..].to_vec();
+
+    let column = grid.iter().map(|row| row[pos.1]).collect::<Vec<u32>>();
+
+    let (up, down) = column.split_at(pos.0);
+    let mut up = up.to_vec();
+    let mut down = down[1..].to_vec();
+    let mut scenic: Vec<u32> = vec![];
+
+
+    up.reverse();
+    left.reverse();
+
+    // println!("height: {}", height);
+    for dir in &[left, right, up, down] {
+        // println!("dir: {:?}", dir);
+        let mut count: u32 = 0;
+        for t in dir {
+            // println!("t: {}", t);
+            if t < &height {
+                count += 1;
+            }
+            if t >= &height {
+                count += 1;
+                break;
+            }
+        }
+
+        scenic.push(count);
+    }
+    let score = scenic.clone().into_iter().reduce(|acc, item| (acc*item)).unwrap();
+    // println!("({}, {}) scenic: {:?} score {}", pos.0, pos.1, scenic, score);
+
+    return score;
+}
+
 fn is_visible(grid: &Vec<Vec<u32>>, pos: (usize, usize)) -> bool {
-    // println!("{} ({}, {})", grid[pos.0][pos.1], pos.0, pos.1);
     let height = grid[pos.0][pos.1];
     let (left, right) = grid[pos.0].split_at(pos.1);
     let left = left.to_vec();
@@ -11,15 +50,10 @@ fn is_visible(grid: &Vec<Vec<u32>>, pos: (usize, usize)) -> bool {
     let up = up.to_vec();
     let down = down[1..].to_vec();
 
-    // println!("up: {:?}", up);
-    // println!("down: {:?}", down);
-    // println!("left: {:?}", left);
-    // println!("right: {:?}", right);
-
-    return (left.iter().all(|h| h < &height)
+    return left.iter().all(|h| h < &height)
         || right.iter().all(|h| h < &height)
         || up.iter().all(|h| h < &height)
-        || down.iter().all(|h| h < &height));
+        || down.iter().all(|h| h < &height);
 }
 
 fn main() {
@@ -31,22 +65,25 @@ fn main() {
         .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect::<Vec<Vec<u32>>>();
 
+    // scenic_score(&grid, (3,2));
+
     let mut count = 0;
 
-    // for row in &grid {
-    //     println!("{:?}", row);
-    // }
-    //
+    let mut highest_scenic = 0;
     for row in 0..grid.len() {
         for col in 0..grid[row].len() {
             if is_visible(&grid, (row, col)) {
                 // println!("({}, {})", row, col);
                 count += 1;
             }
+            let scenic = scenic_score(&grid, (row, col));
+            // println!("{}, {} scenic: {}", row, col, scenic);
+            if scenic > highest_scenic {
+                highest_scenic = scenic;
+            }
         }
     }
-    // 2 2
-    // 3 1
 
     println!("part 1: {}", count);
+    println!("part 2: {}", highest_scenic);
 }
